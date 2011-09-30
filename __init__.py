485119a7288bc320eb63017400e603bc8f9129fd
Oleksandr Pryymak <235@pleso.net>
2011-09-30 16:44:24 +0300

    Code cleanup

--- __init__.py
+++ __init__.py
@@ -1,17 +1,24 @@
 from django.conf import settings
-from hashlib import md5
-from BeautifulSoup import BeautifulSoup, Tag
 from django.template import Template, StringOrigin
+from BeautifulSoup import BeautifulSoup, Tag
+from hashlib import md5
 import inspect, os
-
 try:
     from threading import local
 except ImportError:
     from django.utils._threading_local import local
 
+#Create a global variable that will be visible in modified template render 
+#here we store all metadata about produced output
 globals = local()
 
+###Django template engine monkey-patching
+#we enable meta-data collection in its runtime
+trace_template = """<i>%(file)s</i><br/>
+		    &nbsp;&nbsp;#%(line)s: %(func)s()<br/>
+		    &nbsp;&nbsp;<small>%(code)s</small>"""
 
+#Decorator over original init in order to store the template name
 def enhanced_init(old_init):
     def new_init(self, template_string, origin=None, name='<Unknown Template>'):
         if origin is None:
@@ -21,56 +28,61 @@ def enhanced_init(old_init):
         return old_init(self, template_string, origin, name)
     return new_init
 
+#New renderer method
 def render(self, context):
-    print self.origin, self.name
-    import django
-    _django_path = django.__file__.split('__init__')[0]
-    _introspection_path = __file__.split('__init__')[0]
-    #print _django_path, _introspection_path
+    #print self.origin, self.name
     
+    #Collect pathes to modules that have to be excluded from a trace tree: django & this module
+    import django
+    _exclude_path = (django.__file__.split('__init__')[0] , __file__.split('__init__')[0] )
+    
+    #Inspect the trace tree, collect usuful data
     tree = []
     frame = inspect.currentframe().f_back
-    while frame.f_back and ( frame.f_code.co_filename.startswith(_django_path) or frame.f_code.co_filename.startswith(_introspection_path) ):
-        #print frame.f_code.co_filename
+    #skip excluded and only then gather few hops
+    while frame.f_back and frame.f_code.co_filename.startswith(_exclude_path):
         frame = frame.f_back
     for i in range(1,3):
-        if frame.f_code.co_filename.startswith(_django_path) or frame.f_code.co_filename.startswith(_introspection_path):
+        if frame.f_code.co_filename.startswith(_exclude_path):
             frame = frame.f_back
             continue
-        trace=inspect.getframeinfo(frame)
+        trace = inspect.getframeinfo(frame)
         filename = os.path.relpath(trace.filename, settings.SITE_BASEDIR )
-        formatted = '<i>%s</i><br/>&nbsp;&nbsp;#%s: %s()<br/>&nbsp;&nbsp;<small>%s</small>' % (filename, trace.lineno, trace.function, "<br/>".join(trace.code_context))
+        formatted = trace_template % { 'file': filename, 
+                                       'line': trace.lineno, 
+                                       'func': trace.function, 
+                                       'code': "<br/>".join(trace.code_context)}
         tree.append( formatted.replace("'",'"') )
-        #tree.append(str(inspect.getframeinfo(frame)).replace("'",'"').replace(r"\n",""))
         frame = frame.f_back
-
+ 
+    #add current rendering event to the global metadata with a hash
     info = {'origin': os.path.relpath(str(self.origin), settings.SITE_BASEDIR ), 
             #'name':   str(self.name),
             'tree': "<br/>".join(tree)}
-
     hash = md5(str(info['origin'])).hexdigest()
     globals.tdebug[hash] = info
 
+    #The only line from the original method before patching - the rendering itself
     render = self.nodelist.render(context)
-    #return render
-    soup = BeautifulSoup(render)
+   
+    #introduce a new attribute to each produced tag with a given hash or extend existing
+    #WARNING: if produced HTML will be invalid, BeautifulSoup will try to fix it.
+    soup = BeautifulSoup(render) 
     g = soup.recursiveChildGenerator()
     while True:
         try:
             tag = g.next()
-            if isinstance(tag, Tag):
-                if tag.has_key('dhash'):
-                    if hash not in tag['dhash'].split(' '): 
-                        tag['dhash'] = tag['dhash'] + ' ' + hash
-                else:
-                    tag['dhash'] = hash
-                #pass
+            if not isinstance(tag, Tag):
+                continue
+            if tag.has_key('dhash') and hash not in tag['dhash'].split(' '): 
+                tag['dhash'] = tag['dhash'] + ' ' + hash
+            else:
+                tag['dhash'] = hash
         except StopIteration:
             break    
     return unicode(soup)
-    #return '<span style="border: solid 1px red; display:block;">' + self.nodelist.render(context) + '</span>' + self.name #+'<!--'+ str(self.origin) +'-->'
 
-
+#Monkey-patching itself
 Template.__init__ = enhanced_init(Template.__init__)
 Template.render = render
 
--- middleware.py
+++ middleware.py
@@ -2,16 +2,19 @@ from django_template_introspection import globals
 from django.conf import settings
 import re, os, json
 
+insert_files = ('js/insertion.js', 'js/jquery.jgrowl_minimized.js', 'js/jquery.jgrowl.css')
+
 class TemplateIntrospect(object):
+    #create empty meta-data storage
     def process_request(self, request):
+        globals.tdebug={}
         #globals.request = request
         #globals.user = getattr(request, 'user', None)
-        globals.tdebug={}
 
+    #inject meta-data & javascripts to navigate it into resulting HTML output
     def process_response(self, request, response):
         content = response.content
         head = re.search(u"</head", content, re.I)
-        #index = head.start() if head else 0
         if head:
             index = head.start()
         else:
@@ -19,15 +22,18 @@ class TemplateIntrospect(object):
         response.content =  content[:index] + self.form_introspec_data() + content[index:]
         return response 
 
-    def form_introspec_data(self):
+    #format insetion block
+    @staticmethod
+    def form_introspec_data():
         def read(fname):
             path = os.path.dirname(__file__) 
             f = open( os.path.join(path, fname) , 'r')
             content = f.read()
             f.close()
             return content
-        insertion = read('js/insertion.js')
-        tip = read('js/jquery.jgrowl_minimized.js')
-        tipcss = read('js/jquery.jgrowl.css');
-        return '<script type="text/javascript"> var dhash=' +  json.dumps(globals.tdebug) + '; </script>' + insertion + tip + tipcss
+        return '<script type="text/javascript"> var dhash=' + \
+               json.dumps(globals.tdebug) + \
+               '; </script>' + \
+               reduce(lambda x,y: x+'\n'+y, [read(i) for i in insert_files])
+
 
