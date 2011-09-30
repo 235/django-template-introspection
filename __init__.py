bc6432db953c6de484e1bda5ad141977608aad47
Oleksandr Pryymak <235@pleso.net>
2011-09-30 20:44:12 +0300

    pylint

--- __init__.py
+++ __init__.py
@@ -10,16 +10,22 @@ except ImportError:
 
 #Create a global variable that will be visible in modified template render 
 #here we store all metadata about produced output
-globals = local()
+GLOBALS = local()
 
-###Django template engine monkey-patching
-#we enable meta-data collection in its runtime
-trace_template = """<i>%(file)s</i><br/>
+#Collect paths to modules that have to be excluded from a trace tree: 
+#exclude django & current module
+import django
+EXCLUDE_PATHS = (django.__file__.split('__init__')[0] , 
+                 __file__.split('__init__')[0] )
+#template of each trace chunk
+TRACE_TEMPLATE = """<i>%(file)s</i><br/>
 		    &nbsp;&nbsp;#%(line)s: %(func)s()<br/>
 		    &nbsp;&nbsp;<small>%(code)s</small>"""
 
-#Decorator over original init in order to store the template name
+###Django template engine monkey-patching
+#we enable meta-data collection in its runtime
 def enhanced_init(old_init):
+    """ Decorator over the original init in order to store the template name """
     def new_init(self, template_string, origin=None, name='<Unknown Template>'):
         if origin is None:
             self.origin = StringOrigin(template_string)
@@ -28,56 +34,51 @@ def enhanced_init(old_init):
         return old_init(self, template_string, origin, name)
     return new_init
 
-#New renderer method
 def render(self, context):
+    """ New render that saves metadata and annotate produces tags """
     #print self.origin, self.name
-    
-    #Collect pathes to modules that have to be excluded from a trace tree: django & this module
-    import django
-    _exclude_path = (django.__file__.split('__init__')[0] , __file__.split('__init__')[0] )
-    
     #Inspect the trace tree, collect usuful data
     tree = []
     frame = inspect.currentframe().f_back
     #skip excluded and only then gather few hops
-    while frame.f_back and frame.f_code.co_filename.startswith(_exclude_path):
+    while frame.f_back and frame.f_code.co_filename.startswith(EXCLUDE_PATHS):
         frame = frame.f_back
-    for i in range(1,3):
-        if frame.f_code.co_filename.startswith(_exclude_path):
+    for i in range(1, 3):
+        if frame.f_code.co_filename.startswith(EXCLUDE_PATHS):
             frame = frame.f_back
             continue
         trace = inspect.getframeinfo(frame)
-        filename = os.path.relpath(trace.filename, settings.SITE_BASEDIR )
-        formatted = trace_template % { 'file': filename, 
+        filename = os.path.relpath(trace.filename, settings.SITE_BASEDIR)
+        formatted = TRACE_TEMPLATE % { 'file': filename, 
                                        'line': trace.lineno, 
                                        'func': trace.function, 
                                        'code': "<br/>".join(trace.code_context)}
-        tree.append( formatted.replace("'",'"') )
+        tree.append( formatted.replace("'", '"') )
         frame = frame.f_back
  
     #add current rendering event to the global metadata with a hash
-    info = {'origin': os.path.relpath(str(self.origin), settings.SITE_BASEDIR ), 
+    info = {'origin': os.path.relpath(str(self.origin), settings.SITE_BASEDIR),
             #'name':   str(self.name),
             'tree': "<br/>".join(tree)}
-    hash = md5(str(info['origin'])).hexdigest()
-    globals.tdebug[hash] = info
+    dhash = md5(str(info['origin'])).hexdigest()
+    GLOBALS.tdebug[dhash] = info
 
-    #The only line from the original method before patching - the rendering itself
-    render = self.nodelist.render(context)
+    #The only line from the original method - the rendering itself
+    render_result = self.nodelist.render(context)
    
-    #introduce a new attribute to each produced tag with a given hash or extend existing
-    #WARNING: if produced HTML will be invalid, BeautifulSoup will try to fix it.
-    soup = BeautifulSoup(render) 
-    g = soup.recursiveChildGenerator()
+    #add an attribute to each HTML-tag with a given hash or update existing
+    #WARNING: if the produced HTML is invalid, BeautifulSoup will try to fix it
+    soup = BeautifulSoup(render_result) 
+    tags_gen = soup.recursiveChildGenerator()
     while True:
         try:
-            tag = g.next()
+            tag = tags_gen.next()
             if not isinstance(tag, Tag):
                 continue
-            if tag.has_key('dhash') and hash not in tag['dhash'].split(' '): 
-                tag['dhash'] = tag['dhash'] + ' ' + hash
+            if tag.has_key('dhash') and dhash not in tag['dhash'].split(' '): 
+                tag['dhash'] = tag['dhash'] + ' ' + dhash
             else:
-                tag['dhash'] = hash
+                tag['dhash'] = dhash
         except StopIteration:
             break    
     return unicode(soup)
--- middleware.py
+++ middleware.py
@@ -1,44 +1,54 @@
-from django_template_introspection import globals
-from django.conf import settings
+from django_template_introspection import GLOBALS
+#from django.conf import settings
 import re, os, json
 
-insert_files = ('js/insertion.html', 'js/jquery.jgrowl_minimized.js', 'js/jquery.jgrowl.css')
+INSERT_FILES = ('js/insertion.html', 
+                'js/jquery.jgrowl_minimized.js', 
+                'js/jquery.jgrowl.css')
 
 class TemplateIntrospect(object):
-    #create empty meta-data storage
+    """ Template introspection middleware 
+        flushes metadata storage and
+        inserts new metadata with navigational scripts
+        into produced HTML output"""
+ 
     def process_request(self, request):
-        globals.tdebug={}
-        #globals.request = request
-        #globals.user = getattr(request, 'user', None)
+        """ Create empty meta-data storage """
+        GLOBALS.tdebug = {}
+        #GLOBALS.request = request
+        #GLOBALS.user = getattr(request, 'user', None)
 
-    #inject meta-data & javascripts to navigate it into resulting HTML output
     def process_response(self, request, response):
+        """ Inject meta-data & javascripts to navigate it
+            into resulting HTML output """
         content = response.content
         head = re.search(u"</head", content, re.I)
         if head:
             index = head.start()
         else:
             return response
-        response.content =  content[:index] + self.form_introspec_data() + content[index:]
+        response.content =  content[:index] + \
+                            self.form_introspec_data() + \
+                            content[index:]
         return response 
 
-    #format insetion block
     @staticmethod
     def form_introspec_data():
+        """ Format insetion block """
         def read(fname):
             path = os.path.dirname(__file__) 
-            f = open( os.path.join(path, fname) , 'r')
-            content = f.read()
-            f.close()
+            fil = open( os.path.join(path, fname) , 'r')
+            content = fil.read()
+            fil.close()
             if fname.endswith('.js'):
                 return '<script type="text/javascript">' + content + '</script>'
             elif fname.endswith('.css'):
                 return '<style type="text/css" /> ' + content + '</style>'
             else:
-               return content
+                return content
         return '<script type="text/javascript"> var dhash=' + \
-               json.dumps(globals.tdebug) + \
+               json.dumps(GLOBALS.tdebug) + \
                '; </script>' + \
-               reduce(lambda x,y: x+'\n'+y, [read(i) for i in insert_files])
+               reduce(lambda x,y: x+'\n'+y, [read(i) for i in INSERT_FILES])
 
 
